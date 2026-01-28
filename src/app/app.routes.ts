079a55ea1d63295eb3338d32c416ebe0e114c000
Prince Goutia <goutiaprince10@gmail.com-github@exemple.com>
2026-01-28 15:17:18 +0100

    mise a jour 28/01/2026

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,23 @@
 import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
 import { VehiculesComponent } from './components/vehicules/vehicules.component';
+import { ChauffeursComponent } from './components/chauffeurs/chauffeurs.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { CarburantComponent } from './components/carburant/carburant.component';
+import { MaintenanceComponent } from './components/maintenance/maintenance.component';
+import { DocumentsComponent } from './components/documents/documents.component';
+import { AlertesComponent } from './components/alertes/alertes.component';
 
 export const routes: Routes = [
-    {"path":"",component:VehiculesComponent}
+    {path:'dashboard',component:DashboardComponent},
+    {path:'vehicules',component:VehiculesComponent},
+    {path:'chauffeurs',component:ChauffeursComponent}, 
+    {path:'carburant',component:CarburantComponent},
+    {path:'maintenance',component:MaintenanceComponent},
+    {path:'documents',component:DocumentsComponent},
+    {path:'alertes',component:AlertesComponent}
+    
+
+   
+    
 ];
